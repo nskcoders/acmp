a107c853012a8f1b7121d759a53456d2ee61a213
exp101t <rittergerman2000@yandex.ru>
2017-06-05 17:47:14 +0700

    Update 0131.cpp

--- exp101t/0131.cpp
+++ exp101t/0131.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <utility>
 
+int main(){
+    int V, S, N; 
+    std::pair<int, int> max = {-1, -1};
+    scanf("%i", &N);
+    for(int i = 0; i < N; i++){
+        scanf("%i%i", &V, &S);
+        if(S && V > max.second) max = {i, V};
+    }
+    
+    printf("%i", max.first != -1 ? (max.first + 1) : -1);
+}
